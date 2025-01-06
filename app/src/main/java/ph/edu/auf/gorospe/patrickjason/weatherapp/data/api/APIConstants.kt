354e2e52c7fa55a238b4d86a0911ccea47341d8c
Jason Gorospe <9.pjgorospe@gmail.com>
2025-01-06 13:34:20 +0800

    initial commit

--- app/src/main/java/ph/edu/auf/gorospe/patrickjason/weatherapp/data/api/APIConstants.kt
+++ app/src/main/java/ph/edu/auf/gorospe/patrickjason/weatherapp/data/api/APIConstants.kt
@@ -3,7 +3,7 @@ package ph.edu.auf.gorospe.patrickjason.weatherapp.data.api
 object APIConstants {
     const val WEATHER_API_BASE_URL = "https://api.openweathermap.org/data/2.5/"
     const val GEO_API_BASE_URL = "https://api.openweathermap.org/geo/1.0/" // New base URL for geocoding
-    const val API_KEY = "57a14adef0129171c30405da63ce48ca"
+    const val API_KEY = ":P"
     const val WEATHER_ICON_BASE_URL = "https://openweathermap.org/img/wn/"
 
     // Endpoints
